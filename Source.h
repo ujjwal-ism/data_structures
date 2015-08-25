648c9f0edcff926351c72b4cd8447e56b3c7e374
jessehartloff <hartloff@buffalo.edu>
2015-08-25 00:40:55 -0400

    Add some structure

---
+++ Source.h
@@ -0,0 +1,8 @@
+//
+// Created by jesse hartloff on 8/25/15.
+//
+
+#ifndef CSE250_FALL2015_SOURCE_H
+#define CSE250_FALL2015_SOURCE_H
+
+#endif //CSE250_FALL2015_SOURCE_H
---
+++ src/DataStructure.cpp
@@ -0,0 +1,52 @@
+//
+// Created by jesse hartloff on 8/25/15.
+//
+
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <iostream>
+//#include "DataStructure.h"
+
+int main() {
+//    std::cout << "Hello, World!\n";
+//    NewCppClass* object1 = new NewCppClass();
+//    int fart = 0;
+    int n = 500;
+
+    std::cout << "gfgsefgs";
+
+    std::vector<int> intVector(24);
+//    std::array<int, 5> intArray();
+    std::set<int> intSet;
+    std::stack<int> datStack;
+    std::queue<int> datQueue;
+    std::priority_queue<int> intPQ;
+
+//    for (int i = 0; i < n; ++i) {
+        //intPQ.push(std::rand());
+//    }
+
+//    for (int i = 0; i < n; ++i) {
+//        intVector.push_back(i);
+//        intSet.insert(i);
+//        intSet.insert(i);
+//        datStack.push(i);
+//        datQueue.push(i);
+//
+////        std::cout << i << ": " << intVector.capacity() << "\n"; // what do you expect the output to be?
+////        std::cout << i << ": " << intSet.size() <<  "\n"; // what do you expect the output to be?
+////        std::cout << i << ": " << datStack.size() <<  "\n"; // what do you expect the output to be?
+////        std::cout << i << ": " << datQueue.size() <<  "\n"; // what do you expect the output to be?
+//        std::cout << i << ": " << intPQ.top() <<  "\n"; // what do you expect the output to be?
+//        intPQ.pop();
+////        fart = object1->fun1(fart);
+////        std::cout << fart << "\n";
+////        std::cout << doItAgain(i) << "\n";
+//    }
+
+//    std::cout << std::to_string(intVector.capacity()) << "\n";
+    //= new vector<int>();
+    return 0;
+}
