4e9705c1a788203945d32f898d29549df9de9c40
ujjwal <ujjwal01kumar@gmail.com>
2016-06-12 20:49:35 +0530

    solution of hw1

--- homework/hw1/hw1.cpp
+++ homework/hw1/hw1.cpp
@@ -1,17 +1,43 @@
 #include "hw1.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	string str;
+	int no_of_char;
+	int no;
+
+	str = getCustomString();
+	cout << str <<endl;
+
+	no_of_char = countChars("Bhaiya ka sab dost naala..",'a');
+	cout << no_of_char << endl;
+
+	no = countCharsCustom('a');
+	cout << no <<endl;
+	return 0;
+}
 
 std::string getCustomString(){
-    // TODO
-    return "a string";
+    
+    return "Ujjwal Kumar 'The SUPREME Leader'";
 }
 
 
 int countChars(std::string words, char character){
-    // TODO
+    int count=0;
+    int i=0;
+    while(words[i]!='\0'){
+    	if(words[i]==character)
+    		count++;
+    	i++;
+    }
+    return count;
 }
 
-
 int countCharsCustom(char character){
-    // TODO
+
+	return countChars(getCustomString(),character);    
 }
 
